0cc375d2529a4653d3c076e7fcab5376459b33cb
serdartas <serdartas@hotmail.com>
2021-12-06 16:57:16 +0100

    Detect a Cycle

--- Interview Preparation Kit/Linked Lists/Linked Lists Detect a Cycle/Solution.java
+++ Interview Preparation Kit/Linked Lists/Linked Lists Detect a Cycle/Solution.java
@@ -1,3 +1,26 @@
-public class Solution {
-    
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head)
+{
+// Fastest way to implement this is to have two pointers where one of them moves faster
+// than the other. If there's no loop, fast one will hit a null point and algorithm will
+// exit. If there's a loop two pointers will eventually match at some point.
+    Node slow = head, fast = head;
+    while (slow != null && fast != null && fast.next != null)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast)
+            return true;
+    }
+    return false;
 }
